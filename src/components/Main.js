a429b5accaf0ca8c5a4f8ad2197bdae1a5b49fe6
Marnie Robinson <marnielane@gmail.com>
2021-09-01 15:28:07 -0600

    image loading attempt

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,21 @@
-
+import image from '../img/clean.jpg'
 
 const Main = () => {
     return (  
         <div className="Main">
         Main
+        { Image }
         </div>
     );
 }
+
+const Image = () => {
+    return (
+        <div className="Image">
+            <img src={require('../img/clean.jpg')} alt="image" height={300} width={300}/>
+        </div>
+    )
+}
+
  
 export default Main;
