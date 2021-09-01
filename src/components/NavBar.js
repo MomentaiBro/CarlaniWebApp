83188dfaccca1a60082bd1b40bc2dc429ebbd970
Alexander Sherwin <alx.sherwin@gmail.com>
2021-09-01 13:46:57 -0600

    save point

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,22 +8,22 @@ const NavBar = () => {
         <img src="Logo.png" alt="Carlani Logo" className={NavStyle.logo}/>
         <ul className={NavStyle.NavList}>
             <li>
-                <NavLink to="/" exact className={ NavStyle.link } activeClassName={ NavStyle.activeHome}>
+                <NavLink to="/" exact className={ NavStyle.link } activeClassName={ NavStyle.active}>
                     Home
                 </NavLink>
             </li>
             <li>
-                <NavLink to="/about" className={ NavStyle.link } activeClassName={ NavStyle.activeAbout}>
+                <NavLink to="/about" className={ NavStyle.link } activeClassName={ NavStyle.active}>
                     About
                 </NavLink>
             </li>
             <li>
-                <NavLink to="/services" className={ NavStyle.link } activeClassName={ NavStyle.activeServices}>
+                <NavLink to="/services" className={ NavStyle.link } activeClassName={ NavStyle.active}>
                     Services
                 </NavLink>
             </li>
             <li>
-                <NavLink to="/contact" className={ NavStyle.link } activeClassName={ NavStyle.activeContact}>
+                <NavLink to="/contact" className={ NavStyle.link } activeClassName={ NavStyle.active}>
                     Contact
                 </NavLink>
             </li>
