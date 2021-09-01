81918cdb46bb07b2a247fb9d7658965e6cbdcd61
Marnie Robinson <marnielane@gmail.com>
2021-09-01 15:14:36 -0600

    created main component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './components/Home'
 import About from './components/About'
 import Services from './components/Services'
 import Contact from './components/Contact'
+import Main from './components/Main'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom' 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Home/>
         <div className="Main">
           <Switch>
+            <Route component={ Main } path="/" exact/>
             <Route component={ About } path="/about"/>
             <Route component={ Services } path="/services"/>
             <Route component={ Contact } path="/contact"/>
