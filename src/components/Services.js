d2ec9e4d1726fc744f0625bb7adea41bd66b692e
Alexander Sherwin <alx.sherwin@gmail.com>
2021-09-01 12:56:35 -0600

    added shadow to home component. added services information.

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,7 +1,37 @@
 const Services = () => {
     return ( 
         <div className="Services Main"> 
-        Services
+            <div>
+                <ul>
+                    <li>
+                        Paint
+                    </li>
+                    <li>
+                        Clean
+                    </li>
+                    <li>
+                        Organized
+                    </li>
+                    <li>
+                        Change Flooring
+                    </li>
+                    <li>
+                        Change Carpet
+                    </li>
+                    <li>
+                        Outside Cleaning
+                    </li>
+                    <li>
+                        Garbage Clean Up
+                    </li>
+                    <li>
+                        Packing Help
+                    </li>
+                    <li>
+                        And More... 
+                    </li>
+                </ul>
+            </div>
         </div>
      );
 }
