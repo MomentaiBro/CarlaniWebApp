f16f107884339af6af7268284f1debea480dceab
Alexander Sherwin <alx.sherwin@gmail.com>
2021-09-14 10:28:37 -0600

    new version

--- src/App.js
+++ src/App.js
@@ -3,21 +3,16 @@ import Home from './components/Home'
 import About from './components/About'
 import Services from './components/Services'
 import Contact from './components/Contact'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom' 
+import NavBar from './components/NavBar'
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Home/>
-        <div className="Main">
-          <Switch>
-            <Route component={ About } path="/about"/>
-            <Route component={ Services } path="/services"/>
-            <Route component={ Contact } path="/contact"/>
-          </Switch>
-          </div>
-      </Router>
+      <NavBar/>
+      <Home/>
+      <About/>
+      <Services/>
+      <Contact/>
     </div>
   );
 }
