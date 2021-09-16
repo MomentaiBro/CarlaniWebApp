d0c17a0194afdf36ac2a8c9548524174adebec73
Alexander Sherwin <alx.sherwin@gmail.com>
2021-09-16 13:06:51 -0600

    added giant ant

--- src/components/About.js
+++ src/components/About.js
@@ -3,13 +3,18 @@ import style from "../modules/About.module.css"
 const About = () => {
     return ( 
         <div className={style.about} >
+            <section >
+                <img
+                    className={style.aboutImg}
+                    src="carlanilogoant.png"
+                    alt="ant with a broom"
+                    />
+            </section>
             <section className={style.description}>
                 <h2>What's Carlani? </h2>
                 <p> Here are Carlani. We care about Carlani.. and THIS is our story...</p>
             </section>
-            <section className={style.aboutImage}>
-
-            </section>
+            
         </div>
      );
 }
