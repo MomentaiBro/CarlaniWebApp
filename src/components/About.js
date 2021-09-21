8846aa8ce982d86328122e7d93df40121ba153c0
Marnie Robinson <marnielane@gmail.com>
2021-09-21 14:30:01 -0600

    yellow blob added to about

--- src/components/About.js
+++ src/components/About.js
@@ -4,8 +4,6 @@ const About = () => {
     return ( 
         <div className={style.about} id="about">
             <section className={style.antSection}>
-            
-                <svg id="visual" viewBox="0 0 150 200" width="150" height="200" version="1.1"><g transform="translate(76.88152945096533 103.06580930618107)"><path d="M48.7 -54.1C60.2 -37.2 64.6 -18.6 62.6 -2C60.6 14.6 52.2 29.2 40.7 43.1C29.2 56.9 14.6 69.9 -6 75.9C-26.5 81.8 -53 80.7 -62.4 66.9C-71.8 53 -64 26.5 -59.5 4.5C-54.9 -17.4 -53.6 -34.9 -44.3 -51.7C-34.9 -68.6 -17.4 -84.8 0.6 -85.4C18.6 -86 37.2 -70.9 48.7 -54.1" fill="#BB004B"></path></g></svg>
                 <img
                     className={style.aboutImg}
                     src="carlanilogoant.png"
@@ -13,6 +11,7 @@ const About = () => {
                     />
             </section>
             <section className={style.description}>
+                <svg id="visual" viewBox="0 0 900 600" width="900" height="600" version="1.1"><g transform="translate(463.3199470949114 280.5049718295511)"><path d="M139.3 -116.5C187.1 -91.5 237.1 -45.7 232.8 -4.2C228.6 37.2 170.1 74.5 122.3 108.6C74.5 142.8 37.2 173.9 -13.1 187C-63.4 200.1 -126.8 195.1 -176.8 161C-226.8 126.8 -263.4 63.4 -259.4 4C-255.4 -55.4 -210.8 -110.8 -160.8 -135.8C-110.8 -160.8 -55.4 -155.4 -4.8 -150.6C45.7 -145.7 91.5 -141.5 139.3 -116.5" fill="#FADB78"></path></g></svg>
                 <h2>What's Carlani? </h2>
                 <p> Here are Carlani. We care about Carlani.. and THIS is our story...</p>
             </section>
