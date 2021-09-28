c6f2a31f072bfcefb9ef10c5c37e65a83bb9df00
Alexander Sherwin <alx.sherwin@gmail.com>
2021-09-28 14:19:08 -0600

    finished base html

--- src/components/Services.js
+++ src/components/Services.js
@@ -3,7 +3,7 @@ import style from '../modules/Services.module.css'
 
 const Services = () => {
 
-    const [service, setService ] = useState([
+    const [services, setService ] = useState([
         {title: "Painting", description: "We paint all the things. Indoor. outdoor. hoboy everything. even your kids.", icon: "./icons/paint.png"},
         {title: "Cleaning", description: "indoor/outdoor/general trash", icon: "./icons/mop.png"},
         {title: "Organizing", description: "Messy Room? We got you. Well help you organize your office, home, life, love life, anything. you name it.", icon: "./icons/house.png"},
@@ -20,47 +20,21 @@ const Services = () => {
         <div className={style.services} id="services"> 
             <div className="serviceContainer">
                 <h1>Services</h1>
-                <ul>
-                    <li className={style.singleservice}>
-                            <img className={style.serviceicon} src=/>
+               
+               { services.map((service) => (
+                    <ul>
+                        <li className={style.singleservice}>
+                            
+                            <img className={style.serviceicon} src={service.icon}/>
 
                             <h2 className={style.servicetitle}> {service.title}</h2>
 
                             <h4>{service.description}</h4>
-                    </li>
-
-
-
-                    {/* <li className={style.singleserviceb}>
-                            <img className={style.serviceicon}  src="./icons/mop.png"/>
-                            <h2 className={style.servicetitle}>Clean</h2>
-                            <h4>so much cleaning.</h4>
-                    </li> */}
-                    <li>
-                        Organized
-                    </li>
-                    <li>
-                        Change Flooring
-                    </li>
-                    <li>
-                        Change Carpet
-                    </li>
-                    <li>
-                        Outside Cleaning
-                    </li>
-                    <li>
-                        Garbage Clean Up
-                    </li>
-                    <li>
-                        Packing Help
-                    </li>
-                    <li>
-                        And More... 
-                    </li>
-                </ul>
+                        </li>
+                    </ul>
+                ))}
             </div>
         </div>
      );
 }
- 
 export default Services;
