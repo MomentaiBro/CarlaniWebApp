08be1daa9707912f8982ef712a51972cff708f72
Marnie Robinson <marnielane@gmail.com>
2021-09-29 12:22:20 -0600

    setstate testimonials

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,18 +1,27 @@
+import { useState } from "react";
 import style from "../modules/Testimonials.module.css"
 
 const Testimonials = () => {
 
     const [ testimonial, setTestimonial ] = useState([
         {name: "Jane", quote: "Carlani is the BEST. I love Carlani!", image: "./img/tony-closeup.jpg"},
+        {name: "Jane", quote: "Carlani is the BEST. I love Carlani!", image: "./img/tony-closeup.jpg"},
     ]);
 
 const Testimonials = () => {
     return ( 
-        <div 
-            className={style.testimonial}
-            id="testimonials"
-        >
+        <div className={style.testimonials} id="testimonials">
             I'm the Testimonial Page!
+            <span className={style.testimonial}>
+                    
+                <img className={style.image} src={testimonial.image}/>
+
+                <h2 className={style.name}>{testimonial.name}</h2>
+
+                <h4>{testimonial.quote}</h4>
+                  
+                </span>
+
         </div>
      );
 }
