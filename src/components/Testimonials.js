2b829b2e8c95cbb3635fee2edec69bd3c9cac06c
Marnie Robinson <marnielane@gmail.com>
2021-09-29 12:38:38 -0600

    testimonial intial styling

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -5,14 +5,13 @@ const Testimonials = () => {
 
     const [ testimonial, setTestimonial ] = useState([
         {name: "Jane", quote: "Carlani is the BEST. I love Carlani!", image: "./img/tony-closeup.jpg"},
-        {name: "Jane", quote: "Carlani is the BEST. I love Carlani!", image: "./img/tony-closeup.jpg"},
+        {name: "John", quote: "Carlani is my FAVORITe. I highly recommend Carlani!", image: "./img/tony-closeup.jpg"},
     ]);
 
-const Testimonials = () => {
     return ( 
         <div className={style.testimonials} id="testimonials">
             I'm the Testimonial Page!
-            <span className={style.testimonial}>
+            <div className={style.testimonial}>
                     
                 <img className={style.image} src={testimonial.image}/>
 
@@ -20,10 +19,10 @@ const Testimonials = () => {
 
                 <h4>{testimonial.quote}</h4>
                   
-                </span>
+            </div>
 
         </div>
      );
 }
- 
+
 export default Testimonials;
