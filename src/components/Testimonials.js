c579e3f7bd552ad672da81f1a64d54f4d137d7a7
Marnie Robinson <marnielane@gmail.com>
2021-09-29 12:09:57 -0600

    testimonial state setup

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,6 +1,12 @@
 import style from "../modules/Testimonials.module.css"
 
 const Testimonials = () => {
+
+    const [ testimonial, setTestimonial ] = useState([
+        {name: "Jane", quote: "Carlani is the BEST. I love Carlani!", image: "./img/tony-closeup.jpg"},
+    ]);
+
+const Testimonials = () => {
     return ( 
         <div 
             className={style.testimonial}
