817354fcddc36d517869d8016389a135aef02d7f
Marnie Robinson <marnielane@gmail.com>
2021-09-30 13:08:41 -0600

    testimonial mess around

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -4,20 +4,20 @@ import style from "../modules/Testimonials.module.css"
 const Testimonials = () => {
 
     const [ testimonial, setTestimonial ] = useState([
-        {name: "Jane", quote: "Carlani is the BEST. I love Carlani!", image: "./img/tony-closeup.jpg"},
-        {name: "John", quote: "Carlani is my FAVORITe. I highly recommend Carlani!", image: "./img/tony-closeup.jpg"},
+        {image: "./img/tony-closeup.jpg", name: "Jane", quote: "Carlani is the BEST. I love Carlani!" },
+        {image: "./img/tony-closeup.jpg", name: "John", quote: "Carlani is my FAVORITe. I highly recommend Carlani!" },
     ]);
 
     return ( 
         <div className={style.testimonials} id="testimonials">
             I'm the Testimonial Page!
-            <div className={style.testimonial}>
-                    
-                <img className={style.image} src={testimonial.image}/>
+            <div className={style.testimonial}>I am a picture
+                <div className={style.pic}>
+                    <img className={style.image} src={testimonial.image}/>
+                </div>   
+                <h2 className={style.name}>{testimonial.name}persons name</h2>
 
-                <h2 className={style.name}>{testimonial.name}</h2>
-
-                <h4>{testimonial.quote}</h4>
+                <h4>{testimonial.quote}this is where the quote should go</h4>
                   
             </div>
 
