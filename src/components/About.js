bab771bf9422f2147f9850e9b4fa9084b13b6716
Alexander Sherwin <alx.sherwin@gmail.com>
2021-10-12 13:28:10 -0600

    just about finsihed the about section

--- src/components/About.js
+++ src/components/About.js
@@ -12,7 +12,11 @@ const About = () => {
             </section>
             <section className={style.description}>
                 <h1>What's Carlani? </h1>
-                <p>Carlani, LLC was founded with a passionate drive to bring our family-owned business and values to your home improvement and remodeling needs.  Providing solutions to homeowners during their home transitions whether is selling, renting, or remodeling.   We understand that having a construction liaison responsible for the construction project can save clients precious time and eliminate stress, by utilizing a liaison for your remodeling needs will minimize time, frustration and guess work from finding trustworthy, loyal and honest contractors/subcontractors.  We focus on bringing the final finishes to your home to create the look you need for your project whatever the transition may be.  Finishes such as painting, changing fixtures, flooring, carpet cleaning or changing, installing tile, and more are some of the services we offer.</p>
+                <p>Carlani, LLC was founded with a passionate drive to bring our family-owned business and values to your home improvement and remodeling needs.  Providing solutions to homeowners during their home transitions whether is selling, renting, or remodeling.   </p>
+                <br/>
+                <p>We understand that having a construction liaison responsible for the construction project can save clients precious time and eliminate stress, by utilizing a liaison for your remodeling needs will minimize time, frustration and guess work from finding trustworthy, loyal and honest contractors/subcontractors.  </p>
+                <br/>
+                <p>We focus on bringing the final finishes to your home to create the look you need for your project whatever the transition may be.  Finishes such as painting, changing fixtures, flooring, carpet cleaning or changing, installing tile, and more are some of the services we offer.</p>
                 <br/>
             </section>
             
