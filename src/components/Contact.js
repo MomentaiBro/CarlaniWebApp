996fe293ee1f4d0d25246099416c2dfbe37d9e0a
Alexander Sherwin <alx.sherwin@gmail.com>
2021-10-13 15:47:32 -0600

    almost lost all my work. might die. I think we are ok now

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,19 +1,21 @@
-import style from '../modules/Contact.module.css'
+import '../modules/Contact.module.css'
+
+
 const Contact = () => {
     return ( 
 
-        <div>
+        <div id="contact">
             <h1>Contact Us</h1>
             <form class="cf">
-            <div class="half left cf">
-                <input type="text" id="input-name" placeholder="Name"/>
-                <input type="email" id="input-email" placeholder="Email address"/>
-                <input type="text" id="input-subject" placeholder="Subject"/>
-            </div>
-            <div class="half right cf">
-                <textarea name="message" type="text" id="input-message" placeholder="Message"></textarea>
-            </div>  
-            <input type="submit" value="Submit" id="input-submit"/>
+                <div class="half left cf">
+                    <input type="text" id="input-name" placeholder="Name"/>
+                    <input type="email" id="input-email" placeholder="Email address"/>
+                    <input type="text" id="input-subject" placeholder="Subject"/>
+                </div>
+                <div class="half right cf">
+                    <textarea name="message" type="text" id="input-message" placeholder="Message"></textarea>
+                </div>  
+                    <input type="submit" value="Submit" id="input-submit"/>
             </form>
         </div>
 
