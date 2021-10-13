6efe6a4036e868259a982fa332216f15bb050c4e
Alexander Sherwin <alx.sherwin@gmail.com>
2021-10-13 16:43:21 -0600

    these bitches dont even know

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,4 @@
-import '../modules/Contact.module.css'
+import style from '../modules/Contact.module.css'
 
 
 const Contact = () => {
@@ -8,15 +8,17 @@ const Contact = () => {
 
     return ( 
 
-        <div id="contact">
-            <h1>Contact Us</h1>
-            <form class="cf">
+        <div className={style.contact} id="contact">
+            <div className={style.contact_H1}> 
+                <h1>Contact Us</h1>
+            </div>
+            <form className={style.cf}>
                 <div class="half left cf">
                     <input type="text" id="input-name" placeholder="Name"/>
                     <input type="email" id="input-email" placeholder="Email address"/>
                     <input type="text" id="input-subject" placeholder="Subject"/>
                 </div>
-                <div class="half right cf">
+                <div className={`${style.half} ${style.right} ${style.cf}`}>
                     <textarea name="message" type="text" id="input-message" placeholder="Message"></textarea>
                 </div>  
                     <input type="submit" value="Submit" id="input-submit"/>
