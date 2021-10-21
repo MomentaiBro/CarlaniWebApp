f7651e023496122aed89ee30c16b6fa9ba444027
Alexander Sherwin <alx.sherwin@gmail.com>
2021-10-21 14:45:57 -0600

    jushed up services and remade the descriptions

--- src/components/Services.js
+++ src/components/Services.js
@@ -4,12 +4,12 @@ import style from '../modules/Services.module.css'
 const Services = () => {
 
     const [services, setService ] = useState([
-        {title: "Painting", description: "We paint all the things. Indoor. outdoor. hoboy everything", icon: "./icons/paint.png"},
-        {title: "Cleaning", description: "indoor/outdoor/general blah blah blah blah blah blah  blah", icon: "./icons/mop.png"},
-        {title: "Organizing", description: "Messy Room? We got you. Well help you organize your office, home, life, love life", icon: "./icons/house.png"},
-        {title: "Flooring", description: "Wow I love floor sm. Can't lay down enough flooring to cover my love for flooring.", icon: "./icons/flooring.png"},
-        {title: "Packing", description: "moving? Someone died of covid and left you a bunch of stuff? We can help!!!", icon: "./icons/shipping.png"},
-        {title: "Trash Pick Up", description: "Bruh you got trash. we got arms and energy. let us take care of it for you", icon: "./icons/trashcan.png"},
+        {title: "Painting", description: "If there are walls that need some color -- we can paint 'em!", icon: "./icons/paint.png"},
+        {title: "Cleaning", description: "Messy Rooms? Need your house to get ready for showings? We've got a knack for cleaning.", icon: "./icons/mop.png"},
+        {title: "Organizing", description: "We all get a little disorganized from time to time. Let us help organize your office, home, garage, etc.", icon: "./icons/house.png"},
+        {title: "Flooring", description: "Got some flooring you want to get rid of? Maybe need some help instaling your new hardwood floors? We got you!", icon: "./icons/flooring.png"},
+        {title: "Packing", description: "Wow. Moving can be so daunting. Luckily, Carlani has experience helping home owners get ready for the big move!", icon: "./icons/shipping.png"},
+        {title: "Trash Pick Up", description: "Moving and need help getting rid of trash? Let Carlani help you with that so you dont have to worry.", icon: "./icons/trashcan.png"},
     ]);
 
     return ( 
